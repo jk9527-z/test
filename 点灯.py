923c5104e7d236c8c7b789edc3dd2c97f7e2cb50
me <1491349413@qq.com>
2021-05-09 15:47:03 +0800

    点灯.py

---
+++ 点灯.py
@@ -0,0 +1,9 @@
+from fpioa_manager import fm
+from Maix import GPIO
+
+# 13 - green  12 - blue  14 - red
+io_red = 13
+fm.register(io_red, fm.fpioa.GPIO0)
+
+led_r=GPIO(GPIO.GPIO0, GPIO.OUT)
+led_r.value(0)
